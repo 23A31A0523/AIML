5b5f0e89de165bc55c3ae01e01850da2e215877b
Kalyani Satya Harshitha Neela <kalyanineela18@gmail.com>
2026-09-18 21:30:46 +0530

    Learn functions concept

---
+++ Python basics/functions.py
@@ -0,0 +1,27 @@
+# def greet():
+#     print("Welcome to AI/ML!")
+# greet()
+
+
+# def greet_user(name):
+#     print(f"Hello {name}, welcome to AI/ML!")
+# greet_user("Kal")
+
+
+# def check_accuracy(acc):
+#     if acc >= 90:
+#         print("High accuracy")
+#     elif acc >= 80:
+#         print("Good accuracy")
+#     else:
+#         print("Low accuracy")
+# check_accuracy(92)
+# check_accuracy(85)
+# check_accuracy(74)
+
+
+def calculate_average(s1,s2,s3):
+    avg = (s1 + s2 + s3) / 3
+    return avg
+average = calculate_average(80,90,100)
+print(average)
